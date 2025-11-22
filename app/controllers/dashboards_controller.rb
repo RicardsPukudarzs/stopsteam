ef705cec0bdcb73efc18a5a5845ef44b41f0719a
RicardsPukudarzs <richards.pukudarzs@gmail.com>
2025-11-22 12:17:06 +0200

    style spotify artists view

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,15 +5,27 @@ class DashboardsController < ApplicationController
     spotify_user = RSpotify::User.new(session[:spotify_auth])
 
     @top_artists_all_time = spotify_user.top_artists(limit: 50, time_range: 'long_term').map do |artist|
-      { name: artist.name, id: artist.id, image: artist.images.first&.[]('url') }
+      { name: artist.name, image: artist.images.first&.[]('url') }
     end
 
     @top_artists_last_6_months = spotify_user.top_artists(limit: 50, time_range: 'medium_term').map do |artist|
-      { name: artist.name, id: artist.id, image: artist.images.first&.[]('url') }
+      { name: artist.name, image: artist.images.first&.[]('url') }
     end
 
     @top_artists_last_4_weeks = spotify_user.top_artists(limit: 50, time_range: 'short_term').map do |artist|
-      { name: artist.name, id: artist.id, image: artist.images.first&.[]('url') }
+      { name: artist.name, image: artist.images.first&.[]('url') }
+    end
+
+    @top_songs_all_time = spotify_user.top_tracks(limit: 50, time_range: 'long_term').map do |track|
+      { name: track.name, album: track.album.name, image: track.album.images.first&.[]('url') }
+    end
+
+    @top_songs_last_6_months = spotify_user.top_tracks(limit: 50, time_range: 'medium_term').map do |track|
+      { name: track.name, album: track.album.name, image: track.album.images.first&.[]('url') }
+    end
+
+    @top_songs_last_4_weeks = spotify_user.top_tracks(limit: 50, time_range: 'short_term').map do |track|
+      { name: track.name, album: track.album.name, image: track.album.images.first&.[]('url') }
     end
   end
 end
